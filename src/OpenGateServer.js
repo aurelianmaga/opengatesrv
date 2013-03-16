5ddcf9ef96a1f298ab448b9e662109dff7a5bba3
Andrei Elvedi <andrei.elvedi@fortech.ro>
2013-03-16 18:59:15 +0200

    master

--- src/OpenGateServer.js
+++ src/OpenGateServer.js
@@ -5,25 +5,9 @@ var url = require('url');
 var qs = require('querystring');
 //var cafPiFace = require('caf_piface')
 //var Relay = require('./Relay');
-var ldapConnection = require('./LdapConnection');
+var LdapConnection = require('./LdapConnection');
 
-var garageGate = new Relay({
-  port: 0,
-  RelayOnValue: 1,
-  RelayOffValue: 0,
-  write: function(state, port) {
-    piFaceBoard = new cafPiFace.PiFace();
-    piFaceBoard.init();
-    piFaceBoard.write(state, port);
-    piFaceBoard.shutdown()
-  }
-});
- 
-var sample = new LdapConnection('ldapurl');
-sample.connectUser('user','pswd', function(state){
-	console.log("ldap state:" + state);
-});
- 
+  
 http.createServer(function (request, response) {
  
     console.log('request starting...'+request.url);
@@ -35,12 +19,20 @@ http.createServer(function (request, response) {
 	 var userName = q_params.u;
 	 var password = q_params.p;
 	 
+	 if(userName == undefined || password === undefined)
+	 {
+			response.writeHead(500, {'Content-Type': 'text/plain'}); 
+			response.write("Not OK auth for : " + userName + "\n");
+			response.end();
+		return;
+	 }
+	 
 	 console.log('Console ' + userName + "--" + password);	 
 	  
 	 // Compute distance
 	 
 	 // LDAP
-	 var ldapConnection = new LdapConnection('ldapurl');
+	 var ldapConnection = new LdapConnection('url');
 	 ldapConnection.connectUser(userName, password, function(canEnter){
 		console.log("ldap state:" + canEnter);
 		if (canEnter) {
