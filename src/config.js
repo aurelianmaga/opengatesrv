029117ceee3c6f32964de2e55a34b3ee508769bc
flaviaemi <flavia.emiliana@gmail.com>
2013-03-17 12:31:47 +0200

    Distance

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
 config.ldapUrl = 'ldaps://ldap.fortech.ro:636';
-config.minDistance = 100;
+config.minDistance = 400;
 config.Gate = {latitude: 46.75429, longitude: 23.59};
 config.gateOpenTime = 5000;
 
